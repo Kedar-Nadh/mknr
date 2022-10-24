c4a3a8826ef776b06e095cd3356978efb625e792
Kedar-Nadh <mknr2001@gamil.com>
2022-10-24 17:03:24 +0530

    Deploy website

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer(){
             <div className="social-icons">
                 <a href="https://www.linkedin.com/in/kedar-nadh"><img src={require("../icons/linkedin.svg").default} alt="linkedin"/></a>
                 <a href="https://github.com/Kedar-Nadh"><img src={require("../icons/github.svg").default} alt="github"/></a>
-                <a href="https://www.google.com"><img src={require("../icons/telegram.svg").default} alt="telegram"/></a>
+                <a href="https://t.me/Kedar_Nadh"><img src={require("../icons/telegram.svg").default} alt="telegram"/></a>
             </div>
             <p className="copyrights">&copy;2022 by kedarnadh</p>
         </div>
