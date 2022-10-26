7176c03a5e9142f5c7f089fa7d0cbddc3616200a
Kedar-Nadh <93716743+Kedar-Nadh@users.noreply.github.com>
2022-10-26 11:28:07 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,11 @@
 import React from "react"
-import { BrowserRouter, Link } from "react-router-dom"
 import "../App.css"
 
 export default function Navbar(props){
     return(
-        <BrowserRouter>
         <div className="Navbar">
-            <Link href="/" className="logo" data-hover="Moram KedarNadh Reddy">MKNR</Link>
+            <a href="/" className="logo" data-hover="Moram KedarNadh Reddy">MKNR</a>
             <img onClick={props.handleClick} src={require("../icons/menu.svg").default} alt="menu"/>
         </div>
-        </BrowserRouter>
     )
 }
