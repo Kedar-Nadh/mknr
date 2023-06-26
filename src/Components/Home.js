7ef17ff67d26fd6e100647e3d5afc8c0fe7e6614
Kedar-Nadh <93716743+Kedar-Nadh@users.noreply.github.com>
2023-06-26 08:44:22 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,7 +8,7 @@ export default function Home(){
                 <p className="Home--text-intro">Hi there!</p>
                 <p className="Home--text-intro">I'm KedarNadh 👋🏻,</p>
                 <p className="Home--text-about">I'm a Front End Developer 👨🏻‍💻 interested in web design 👨🏻‍🎨 and web development ⚙️.</p>
-                <p className="Home--text-about">I'm currently pursuing Electronics and Communication Engineering from Sri Venkateswara College of Engineering in Tirupati.</p>
+                <p className="Home--text-about">I pursued Electronics and Communication Engineering at Sri Venkateswara College of Engineering in Tirupati.</p>
             </div>
             <div className="Home--avatar"><img className="Home--avatar-image" src={require("../icons/home-avatar.svg").default} alt="Boy Vector img"/></div>
         </div>
