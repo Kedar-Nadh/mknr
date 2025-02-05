bde584c876212643baf00ca4c1ef1533f7e57c73
Kedarnadh <93716743+Kedar-Nadh@users.noreply.github.com>
2025-02-05 22:21:22 +0530

    Update About.js
    Update about description, skills and added working experience

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,17 +7,52 @@ export default function About(){
             <p className="About--title">About me...</p>
             <div className="About--image-box"><img className="About--image" src={require("../images/kedarnadh.png")} alt="kedarnadh" /></div>
             <h2 className="About--name">Moram KedarNadh Reddy</h2>
-            <p className="About--text">I'm a front-end developer from Tirupati, Andhra Pradesh, India. I work on front-end development and design projects. I enjoy creating visually appealing websites and web apps.</p>
-            <p className="About--text">When I'm not coding, I enjoy watching movies or browsing YouTube.</p>
+            <p className="About--text">Originally from the land of temples, Tirupati 🛕, now living the tech life in Bengaluru 💻🌆.</p>
+            <p className="About--text">I spend my days coding and creating apps like a tech wizard 🧙‍♂️💻.</p>
+            <p className="About--text">When I'm not coding, you’ll find me:</p>
+            <ul className="About--text">
+              <li>Lost in a movie marathon 🎞️🍿</li>
+              <li>Diving deep into random YouTube videos like a curious explorer 🔍🌍</li>
+              <li>Hanging out with family and friends 👨‍👩‍👧‍👦🤗</li>
+            </ul>
+            <p className="About--side-heading">Working Experience -</p>
+            <div className="About--experience">
+                <div className="About--experience-item">
+                    <div>
+                        <p className="About--experience-item-company">Torry Harris Integration Solutions</p>
+                        <p className="About--experience-item-role">Associate Software Engineer</p>
+                    </div>
+                    <p className="About--experience-item-duration">(Feb 2024 – Present)</p>
+                    <p className="About--experience-item-description">
+                        Worked on creating data pipelines using Ab Initio, improving performance and reducing memory usage. Collaborated with different teams to deliver high-quality solutions that met everyone's needs.
+                    </p>
+                </div>
+                <div className="About--experience-item">
+                    <div>
+                        <p className="About--experience-item-company">Torry Harris Integration Solutions</p>
+                        <p className="About--experience-item-role">Associate Software Engineer Trainee</p>
+                    </div>
+                    <p className="About--experience-item-duration">(Aug 2023 – Feb 2024)</p>
+                    <p className="About--experience-item-description">
+                        Built a full-stack app called Kupanify, where users can share and access unused coupons. Deployed the app on the cloud and gained experience with various development tools and cloud services to help scale the app.
+                    </p>
+                </div>
+            </div>
             <p className="About--side-heading">Tools I use -</p>
             <ul className="About--skills">
                 <li className="About--skills-item">Python</li>
+                <li className="About--skills-item">MySQL</li>
+                <li className="About--skills-item">Swift</li>
+                <li className="About--skills-item">SwiftUI</li>
                 <li className="About--skills-item">HTML</li>
                 <li className="About--skills-item">CSS</li>
                 <li className="About--skills-item">BootStrap</li>
                 <li className="About--skills-item">JavaScript</li>
                 <li className="About--skills-item">ReactJs</li>
+                <li className="About--skills-item">Django</li>
                 <li className="About--skills-item">Figma</li>
+                <li className="About--skills-item">Docker</li>
+                <li className="About--skills-item">Kubernetes</li>
             </ul>
             <p className="About--side-heading">Educational History -</p>
             <div className="About--education">
