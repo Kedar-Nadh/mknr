1615e67099977476c9a5bf252a1014b6701665a3
Kedarnadh <93716743+Kedar-Nadh@users.noreply.github.com>
2025-02-05 23:52:56 +0530

    Update Home.js
    Update line height issue in home page description

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,8 +7,8 @@ export default function Home(){
             <div className="Home--text">
                 <p className="Home--text-intro">Hi there!</p>
                 <p className="Home--text-intro">I'm KedarNadh 👋🏻,</p>
-                <p className="Home--text-about">Tech enthusiast with a passion for software development, currently working as an Associate Software Engineer at Torry Harris Integration Solutions (THIS) 🚀💻.</p>
-                <p className="Home--text-about">Exploring iOS development with SwiftUI 📱, while skilled in full-stack development 🌐, data engineering 📊, and design 🎨.</p>
+                <p className="Home--text-about">Tech enthusiast with a passion for software development, currently working as an Associate Software Engineer at Torry Harris Integration Solutions (THIS) <span className="emoji">🚀💻</span></p>
+                <p className="Home--text-about">Exploring iOS development with SwiftUI <span className="emoji">📱</span>, while skilled in full-stack development <span className="emoji">🌐</span>, data engineering <span className="emoji">📊</span>, and design <span className="emoji">🎨</span>.</p>    
             </div>
             <div className="Home--avatar"><img className="Home--avatar-image" src={require("../icons/home-avatar.svg").default} alt="Boy Vector img"/></div>
         </div>
