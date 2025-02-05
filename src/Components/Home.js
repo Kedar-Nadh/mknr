b91e8d160c27aa22b9f779221860877db35921c6
Kedarnadh <93716743+Kedar-Nadh@users.noreply.github.com>
2025-02-05 21:01:38 +0530

    Update Home.js
    Added updated description in Home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,8 +7,8 @@ export default function Home(){
             <div className="Home--text">
                 <p className="Home--text-intro">Hi there!</p>
                 <p className="Home--text-intro">I'm KedarNadh 👋🏻,</p>
-                <p className="Home--text-about">I'm a Front End Developer 👨🏻‍💻 interested in web design 👨🏻‍🎨 and web development ⚙️.</p>
-                <p className="Home--text-about">I pursued Electronics and Communication Engineering at Sri Venkateswara College of Engineering in Tirupati.</p>
+                <p className="Home--text-about">Tech enthusiast with a passion for software development, currently working as an Associate Software Engineer at Torry Harris Integration Solutions (THIS) 🚀💻.</p>
+                <p className="Home--text-about">Exploring iOS development with SwiftUI 📱, while skilled in full-stack development 🌐, data engineering 📊, and design 🎨.</p>
             </div>
             <div className="Home--avatar"><img className="Home--avatar-image" src={require("../icons/home-avatar.svg").default} alt="Boy Vector img"/></div>
         </div>
