317188c666f7b6fe6f4053d5b09b5153f834f955
Kedarnadh <93716743+Kedar-Nadh@users.noreply.github.com>
2025-02-08 17:29:19 +0530

    Update Menu.js

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -13,7 +13,7 @@ export default function Menu(props){
                     <li><Link to="/" className="menu-item" onClick={props.handleClick}>Home</Link></li>
                     <li><Link to="/about" className="menu-item" onClick={props.handleClick}>About</Link></li>
                     <li><Link to="/work" className="menu-item" onClick={props.handleClick}>Work</Link></li>
-                    <li><a href="https://docs.google.com/document/d/1pr9mZFbFSLbO_h794TFH12hdl-m2p1VZ/edit?usp=sharing&ouid=100123059827996591968&rtpof=true&sd=true" className="menu-item">Resume</a></li>
+                    <li><a href="https://docs.google.com/document/d/1g0ruuWqKptW-1MiL4SYHbT_xHC2xXP8JpJpTrgHabGs/edit?usp=sharing" className="menu-item">Resume</a></li>
                     <li><Link to="/contact" className="menu-item" onClick={props.handleClick}>Contact</Link></li>
                 </ul>
             </div>
